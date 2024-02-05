5668fe395dfd50b4a51fd0bfa2cb199559d05743
Samuel Subielos <ssubielo@3C7.42angouleme.fr>
2024-02-05 22:34:24 +0100

    fix node / node map on going

--- lem-in.c
+++ lem-in.c
@@ -5,12 +5,12 @@ typedef struct s_node
 {
     int x;
     int y;
-    int id;
+    char *id;
     char type;
     struct s_node **linked_nodes;
 } t_node;
 
-t_node *create_new_node(int x,int y, int id, char type)
+t_node *create_new_node(int x,int y, char *id, char type)
 {
     t_node* new_node = malloc(sizeof(t_node));
     new_node->id = id;
@@ -45,25 +45,67 @@ void add_link(t_node *src_node, t_node *add_node)
         src_node->linked_nodes = malloc(sizeof(t_node*) * 2);
         src_node->linked_nodes[0] = add_node;
         src_node->linked_nodes[1] = NULL;
-
     }
     else
     {
-        t_node  **new_first_node_link = malloc(sizeof(t_node*) * (get_linked_node_len(src_node->linked_nodes) + 1));
+        t_node  **new_src_node_link = malloc(sizeof(t_node*) * (get_linked_node_len(src_node->linked_nodes) + 1));
         while(src_node->linked_nodes[i])
         {
-            new_first_node_link[i] = src_node->linked_nodes[i];
+            new_src_node_link[i] = src_node->linked_nodes[i];
             i++;
         }
-        new_first_node_link[i] = add_node;
-        new_first_node_link[i+1] = NULL;
+        new_src_node_link[i] = add_node;
+        new_src_node_link[i+1] = NULL;
+        free (src_node->linked_nodes);
+        src_node->linked_nodes = new_src_node_link;
     }
+}
+
+int ft_strcomp(char *src, char *dst)
+{
+    int i = 0;
+    while (src[i] || src[i] != '\n')
+    {
+        if(!dst[i] && src[i] == '\n')
+            return 1;
+        if (src[i] != dst[i])
+            return 0;
+        i++;
+    }
+    if (ft_strlen(dst) != i)
+        return 0;
+    return 1;
 }
 
 void link_node(t_node *first_node, t_node *second_node)
 {
     add_link(first_node, second_node);
     add_link(second_node, first_node);
+}
+
+t_node *find_node(t_node **nodes, char *id)
+{
+    int i = 0;
+    while(nodes[i])
+    {
+        if (ft_strcomp(nodes[i]->id, id) == 0)
+            return (nodes[i]);
+    }
+    return(NULL);
+}
+
+t_node *create_node_map(char **link, t_node **nodes)
+{
+    int i = 0;
+
+    while (link[i])
+    {
+        t_node *first_link = find_node(nodes, link[i][0]);
+        t_node *second_link = find_node(nodes, link[i][1]);
+        if (!first_link || !second_link)
+            return(NULL);
+        link_node(first_link, second_link);
+    }
 }
 
 int main()
